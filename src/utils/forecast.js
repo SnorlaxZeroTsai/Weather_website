fb457a5ee994178f637a719beb0534fe95856c31
Zero Tsai <94668573+SnorlaxZeroTsai@users.noreply.github.com>
2022-01-25 11:45:41 +0800

    Update forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,17 +1,7 @@
-//
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
-
 const request = require('request')
 
 const forecast = (latitude, longtitude, callback) => {
-    const url = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + '&lon='+ longtitude +"&appid=5f96cadd75e5c930a8330edef7cefca5"
+    const url = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + '&lon='+ longtitude +"&appid={my_token}"
     //console.log(url)
     request({url, json: true}, (error, {body}) =>{
         if (error){
