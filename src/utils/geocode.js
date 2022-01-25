cba3123051cd6f230541b3f02e6795ed37f943f4
Zero Tsai <94668573+SnorlaxZeroTsai@users.noreply.github.com>
2022-01-25 11:45:08 +0800

    Update geocode.js

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,6 +1,6 @@
 const request = require('request')
 const geocode = (address, callback) => {
-    const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + address + ".json?types=address&access_token=pk.eyJ1IjoiemVybzAzMDgiLCJhIjoiY2t5b2RiYTMxMzVtaDMzcWhzeWFzbmx4cyJ9.Dd47upIM3njHDAWLIVDVbQ"
+    const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + address + ".json?types=address&access_token={my_token}"
     request({url, json: true},(error, {body}) =>{
         if (error){
             callback("Unable connect")
